5886fb7a550e49bc64f7c70c3e2517565af080bf
SiliconSloth <siliconsloth64@gmail.com>
2020-03-03 16:33:27 +0000

    Remove obsolete Drools helper functions

--- src/siliconsloth/miniruler/Main.kt
+++ src/siliconsloth/miniruler/Main.kt
@@ -2,12 +2,9 @@ package siliconsloth.miniruler
 
 import com.mojang.ld22.Game
 import kotlinx.coroutines.runBlocking
-import org.kie.api.KieServices
-import org.kie.api.runtime.KieSession
 import siliconsloth.miniruler.engine.RuleEngine
 import siliconsloth.miniruler.rules.memoryRules
 import siliconsloth.miniruler.rules.menuRules
-import java.lang.Exception
 
 fun main() {
     runBlocking {
@@ -30,27 +27,3 @@ fun <T: Spatial> screenFilter(cameraX: () -> Int, cameraY: () -> Int) = AreaFilt
         { (cameraY() / 16) * 16 },
         { ((cameraY() + Game.HEIGHT) / 16) * 16 - 1 }
 )
-
-fun KieSession.update(fact: Fact) =
-        update(getFactHandle(fact), fact)
-
-fun KieSession.delete(fact: Fact) =
-        try {
-            delete(getFactHandle(fact))
-        } catch (e: Exception) {
-
-        }
-
-fun KieSession.insertOrUpdate(fact: Fact) {
-    if (getFactHandle(fact) == null) {
-        insert(fact)
-    } else {
-        update(fact)
-    }
-}
-
-fun KieSession.deleteIfPresent(fact: Fact) {
-    if (getFactHandle(fact) != null) {
-        delete(fact)
-    }
-}
