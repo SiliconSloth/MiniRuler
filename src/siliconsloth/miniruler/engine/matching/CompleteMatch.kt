5f2e23851a22c478d6e6d5e74ae8b6bf69462449
SiliconSloth <siliconsloth64@gmail.com>
2020-03-16 20:47:06 +0000

    Add exists()

--- src/siliconsloth/miniruler/engine/matching/CompleteMatch.kt
+++ src/siliconsloth/miniruler/engine/matching/CompleteMatch.kt
@@ -4,6 +4,8 @@ import siliconsloth.miniruler.engine.FactUpdater
 import siliconsloth.miniruler.engine.Rule
 import siliconsloth.miniruler.engine.builders.MatchAtomicBuilder
 import siliconsloth.miniruler.engine.RuleEngine
+import siliconsloth.miniruler.engine.filters.Filter
+import siliconsloth.miniruler.engine.stores.FactStore
 import kotlin.reflect.KClass
 
 class CompleteMatch(rule: Rule): MatchNode(rule), FactUpdater<Any> {
@@ -52,4 +54,12 @@ class CompleteMatch(rule: Rule): MatchNode(rule), FactUpdater<Any> {
     fun maintain(fact: Any) = atomic {
         maintain(fact)
     }
+
+    inline fun <reified T: Any> exists(noinline condition: T.() -> Boolean): Boolean =
+            exists(Filter(condition))
+
+    inline fun <reified T: Any> exists(filter: Filter<T>): Boolean =
+            rule.engine.stores[T::class]?.let {
+                (it as FactStore<T>).retrieveMatching(filter).any()
+            } ?: false
 }
