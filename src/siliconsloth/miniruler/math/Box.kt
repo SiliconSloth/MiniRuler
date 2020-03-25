00865eb8b7836aa08a56bacc54951ca7dd72960b
SiliconSloth <siliconsloth64@gmail.com>
2020-03-25 22:30:27 +0000

    Allow boxes to be defined by any two opposite corners, with padding

--- src/siliconsloth/miniruler/math/Box.kt
+++ src/siliconsloth/miniruler/math/Box.kt
@@ -1,6 +1,12 @@
 package siliconsloth.miniruler.math
 
-class Box(val min: Vector, val max: Vector) {
+import kotlin.math.max
+import kotlin.math.min
+
+class Box(corner1: Vector, corner2: Vector, padding: Int = 0) {
+    val min = Vector(min(corner1.x, corner2.x) - padding, min(corner1.y, corner2.y) - padding)
+    val max = Vector(max(corner1.x, corner2.x) + padding, max(corner1.y, corner2.y) + padding)
+
     operator fun contains(v: Vector) =
             v.x in min.x..max.x && v.y in min.y..max.y
 }
