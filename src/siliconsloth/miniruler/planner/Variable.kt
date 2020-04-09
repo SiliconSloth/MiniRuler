f1e542001dfac884abaa9e9a94ba332eb586d1c4
SiliconSloth <siliconsloth64@gmail.com>
2020-04-10 00:10:49 +0100

    Add comments to Variable.kt

--- src/siliconsloth/miniruler/planner/Variable.kt
+++ src/siliconsloth/miniruler/planner/Variable.kt
@@ -1,3 +1,8 @@
 package siliconsloth.miniruler.planner
 
+/**
+ * A variable in the planning problem of type T.
+ *
+ * @param initializeDomain returns the full domain of the variable (e.g. all positive integers)
+ */
 class Variable<T>(val initializeDomain: () -> Domain<T> = { AnyValue() })
