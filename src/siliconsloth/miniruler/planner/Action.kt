0d2863514aced320772c24b594fa81e8131e391e
SiliconSloth <siliconsloth64@gmail.com>
2020-04-10 00:45:52 +0100

    Add comments to Action.kt

--- src/siliconsloth/miniruler/planner/Action.kt
+++ src/siliconsloth/miniruler/planner/Action.kt
@@ -3,9 +3,11 @@ package siliconsloth.miniruler.planner
 class Action(val name: String, val prerequisite: State, val operations: Map<Variable<*>, Operation<*>>):
         VariableContainer<Operation<*>?> {
 
+    // For VariableContainer
     override val varValues: Map<Variable<*>, Operation<*>>
         get() = operations
 
+    // For VariableContainer; defaults to no operation
     override fun defaultValue(variable: Variable<*>): Operation<*>? =
             null
 
@@ -17,9 +19,9 @@ class Action(val name: String, val prerequisite: State, val operations: Map<Vari
 
     private fun apply(state: State, applyFunc: (Operation<Any>, Domain<Any>) -> Domain<*>): State =
             State(this.zip(state).map { vs -> vs.variable to (
-                    @Suppress("UNCHECKED_CAST")
-                    vs.value1?.let { applyFunc(vs.value1 as Operation<Any>, vs.value2 as Domain<Any>)
-                } ?: vs.value2) }.toMap())
+                    @Suppress("UNCHECKED_CAST") // value1: operation value2: domain
+                    vs.value1?.let { applyFunc(vs.value1 as Operation<Any>, vs.value2 as Domain<Any>) } ?: vs.value2
+                ) }.toMap())
 
     override fun toString(): String {
         return name
