16c4bd2ab2eb6d75d62bfb04a82d946e0e6bd592
SiliconSloth <siliconsloth64@gmail.com>
2020-04-10 01:05:45 +0100

    Add comments to State.kt

--- src/siliconsloth/miniruler/planner/State.kt
+++ src/siliconsloth/miniruler/planner/State.kt
@@ -1,9 +1,17 @@
 package siliconsloth.miniruler.planner
 
+/**
+ * Represents a set of states. Each conceptual state in the set is a tuple of values assigned to each variable
+ * in the planning problem. A State instance represents the cross product of the variable domains it specifies.
+ * Variables in the problem that are not included in the State default to
+ * their full domains given by Variable.initializeDomain().
+ */
 data class State(val variables: Map<Variable<*>, Domain<*>>): VariableContainer<Domain<*>> {
+    // For VariableContainer
     override val varValues: Map<Variable<*>, Domain<*>>
         get() = variables
 
+    // For VariableContainer; defaults to Variable's full domain
     override fun defaultValue(variable: Variable<*>): Domain<*> =
             variable.initializeDomain()
 
@@ -12,6 +20,7 @@ data class State(val variables: Map<Variable<*>, Domain<*>>): VariableContainer<
             State(this.zip(other).map { it.variable to
                     (it.value1 as Domain<Any>).intersect(it.value2 as Domain<out Any>) }.toMap())
 
+    // States that contain NoValue are impossible, as there is a variable that cannot be assigned a value.
     fun isValid(): Boolean =
             !variables.containsValue(NoValue())
 
@@ -22,5 +31,6 @@ data class State(val variables: Map<Variable<*>, Domain<*>>): VariableContainer<
     override fun equals(other: Any?): Boolean =
             other is State && this.zip(other).all { it.value1 == it.value2 }
 
+    // Comparing States is complicated so use a simple, inefficient hash code until something faster is needed.
     override fun hashCode(): Int = 0
 }
