6f585e5e8c8bed605231e044c3508ea5d47671ba
SiliconSloth <siliconsloth64@gmail.com>
2020-06-22 17:01:18 +0100

    Add tracks for matches

--- viewer/src/main/kotlin/siliconsloth/miniruler/timelineviewer/Classes.kt
+++ viewer/src/main/kotlin/siliconsloth/miniruler/timelineviewer/Classes.kt
@@ -4,11 +4,15 @@ enum class MatchState {
     MATCHED, FIRED, DROPPED, ENDED;
 }
 
-data class Match(val rule: String, val bindings: List<String>)
-data class MatchEvent(val match: Match, val state: MatchState, val time: Int)
-data class FactEvent(override val time: Int, val isInsert: Boolean, val maintain: Boolean, val producer: Match?): Track.Event
+data class Match(val rule: String, val bindings: List<String>): Track.Owner {
+    override val label = "$rule: $bindings"
+    override val hue = (rule.hashCode() * 17 % 1000) / 1000f
+}
 
 data class Fact(val fact: String, val factClass: String): Track.Owner {
     override val label = fact
     override val hue = (factClass.hashCode() * 17 % 1000) / 1000f
 }
+
+data class MatchEvent(override val time: Int, val match: Match, val state: MatchState): Track.Event
+data class FactEvent(override val time: Int, val isInsert: Boolean, val maintain: Boolean, val producer: Match?): Track.Event
--- viewer/src/main/kotlin/siliconsloth/miniruler/timelineviewer/TimelineViewer.kt
+++ viewer/src/main/kotlin/siliconsloth/miniruler/timelineviewer/TimelineViewer.kt
@@ -11,8 +11,7 @@ import kotlin.math.max
 
 class TimelineViewer(inputPath: String): JFrame("MiniRuler Timeline Recorder") {
     val matches = mutableListOf<Match>()
-    val matchEvents = mutableSetOf<MatchEvent>()
-    val factTracks = mutableMapOf<Pair<String, String>, Track<Fact, FactEvent>>()
+    val tracks = mutableMapOf<Track.Owner, Track<*,*>>()
 
     val parser = Parser.default()
 
@@ -30,7 +29,7 @@ class TimelineViewer(inputPath: String): JFrame("MiniRuler Timeline Recorder") {
         defaultCloseOperation = EXIT_ON_CLOSE
         layout = BorderLayout()
 
-        val timelinePane = TimelinePane(factTracks.values.toList(), maxTime)
+        val timelinePane = TimelinePane(tracks.values.toList(), maxTime)
         val scrollPane = InteractiveScrollPane(timelinePane)
         scrollPane.viewport.scrollMode = JViewport.SIMPLE_SCROLL_MODE
 
@@ -54,7 +53,7 @@ class TimelineViewer(inputPath: String): JFrame("MiniRuler Timeline Recorder") {
             }
         }
 
-        factTracks.values.forEach { it.finalize(maxTime) }
+        tracks.values.forEach { it.finalize(maxTime) }
     }
 
     fun parseMatchEvent(json: JsonObject) {
@@ -73,8 +72,14 @@ class TimelineViewer(inputPath: String): JFrame("MiniRuler Timeline Recorder") {
             match = matches[id]
         }
 
-        val event = MatchEvent(match, state, json.int("time")!!)
-        matchEvents.add(event)
+        val event = MatchEvent(json.int("time")!!, match, state)
+        val track = tracks.getOrPut(match) { Track<Match, MatchEvent>(match) }
+
+        @Suppress("UNCHECKED_CAST")
+        (track as Track<Match, MatchEvent>).addEvent(event)
+        if (state == MatchState.ENDED) {
+            track.closePeriod()
+        }
     }
 
     fun parseFactEvent(json: JsonObject) {
@@ -82,10 +87,12 @@ class TimelineViewer(inputPath: String): JFrame("MiniRuler Timeline Recorder") {
         val fClass = json.string("class")!!
         val producer = json.int("producer")?.let { matches[it] }
 
+        val owner = Fact(fact, fClass)
         val event = FactEvent(json.int("time")!!, json.boolean("insert")!!, json.boolean("maintain")!!, producer)
-        val track = factTracks.getOrPut(fact to fClass) { Track(Fact(fact, fClass)) }
+        val track = tracks.getOrPut(owner) { Track<Fact, FactEvent>(owner) }
 
-        track.addEvent(event)
+        @Suppress("UNCHECKED_CAST")
+        (track as Track<Fact, FactEvent>).addEvent(event)
         if (!event.isInsert) {
             track.closePeriod()
         }
