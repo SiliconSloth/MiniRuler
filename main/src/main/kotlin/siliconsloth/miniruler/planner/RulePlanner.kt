d2dd2fdcd8db0f03100b546b422a7efa2e8ba12c
SiliconSloth <siliconsloth64@gmail.com>
2020-07-14 22:39:18 +0100

    Write final plan to graph file

--- main/src/main/kotlin/siliconsloth/miniruler/planner/RulePlanner.kt
+++ main/src/main/kotlin/siliconsloth/miniruler/planner/RulePlanner.kt
@@ -1,8 +1,15 @@
 package siliconsloth.miniruler.planner
 
+import it.uniroma1.dis.wsngroup.gexf4j.core.EdgeType
+import it.uniroma1.dis.wsngroup.gexf4j.core.Mode
+import it.uniroma1.dis.wsngroup.gexf4j.core.impl.GexfImpl
+import it.uniroma1.dis.wsngroup.gexf4j.core.impl.StaxGraphWriter
 import siliconsloth.miniruler.engine.RuleEngine
 import siliconsloth.miniruler.engine.matching.CompleteMatch
+import siliconsloth.miniruler.engine.stores.FactStore
 import siliconsloth.miniruler.planner.rules.planningRules
+import java.io.File
+import java.io.FileWriter
 
 class RulePlanner(val engine: RuleEngine, val variables: Array<Variable<*>>, val goal: State) {
     var nextId = 0
@@ -15,15 +22,55 @@ class RulePlanner(val engine: RuleEngine, val variables: Array<Variable<*>>, val
                 start.map { (v,d) -> (d as? Enumeration)?.let { v to SetTo((it.values.first())) } }
                         .filterNotNull().toMap())
 
-        engine.planningRules(this)
-        engine.atomic {
-            insert(Step(state(), initialize!!, start, nextId++))
-            insert(Step(goal, finalize, state(), nextId++))
-        }
+        try {
+            engine.planningRules(this)
+            engine.atomic {
+                insert(Step(state(), initialize!!, start, nextId++))
+                insert(Step(goal, finalize, state(), nextId++))
+            }
 
-        while (engine.allMatches.any { it.state == CompleteMatch.State.MATCHED || it.state == CompleteMatch.State.DROPPED }) {
-            engine.insert("Dummy")
-            engine.delete("Dummy")
+            while (engine.allMatches.any { it.state == CompleteMatch.State.MATCHED || it.state == CompleteMatch.State.DROPPED }) {
+                engine.insert("Dummy")
+                engine.delete("Dummy")
+            }
+
+        } finally {
+            val gexf = GexfImpl()
+            val graph = gexf.graph.apply {
+                defaultEdgeType = EdgeType.DIRECTED
+                mode = Mode.STATIC
+            }
+
+            @Suppress("UNCHECKED_CAST")
+            val nodes = (engine.stores[Step::class] as FactStore<Step>).allFacts().associateWith {
+                graph.createNode().apply {
+                    label = it.toString()
+                }
+            }
+
+            @Suppress("UNCHECKED_CAST")
+            val labels = (engine.stores[Link::class] as FactStore<Link>).allFacts().groupBy { Ordering(it.setter, it.precondition.step) }.mapValues { (_, v) ->
+                v.joinToString(", ") {
+                    it.precondition.variable.toString()
+                }
+            }
+
+            var edgeId = 0
+            @Suppress("UNCHECKED_CAST")
+            (engine.stores[Ordering::class] as FactStore<Ordering>).allFacts().forEach { o ->
+                try {
+                    nodes[o.before]!!.connectTo((edgeId++).toString(), labels[o]
+                            ?: "", EdgeType.DIRECTED, nodes[o.after]!!)
+                } catch (ex: Exception) {
+
+                }
+            }
+
+            val graphWriter = StaxGraphWriter()
+            val file = File("output.gexf")
+            FileWriter(file, false).use {
+                graphWriter.writeToStream(gexf, it, "UTF-8")
+            }
         }
     }
 
